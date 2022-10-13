3a702bd97fa5dc0fe50f0fc15e2bb1b2294b4f5f
Kyrylo Davydovskyi <kirill.davidovskiy.work@gmail.com>
2022-10-14 02:14:45 +0300

    add task #5

---
+++ src/test/java/ndr_masters/SquareRecursiveCalculatorTests.java
@@ -0,0 +1,32 @@
+package ndr_masters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class SquareRecursiveCalculatorTests {
+
+    public static Stream<Arguments> testArguments() {
+        return Stream.of(
+                Arguments.of(2, 1.9615705608064609),
+                Arguments.of(1, 1.8477590650225735),
+                Arguments.of(0, 1.4142135623730951)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testArguments")
+    @Tag("Demonstration")
+    public void demonstrateTask4(int x, double expected) {
+        log.info("Task #4. Testing value: {}, Expected: {}", x, expected);
+        var actual = SquareRecursiveCalculator.calculate(x);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
